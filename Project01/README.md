45c0754886efe1c767fc80f9a15923e4f2ba4862
Hailan Kan <kanh3@mcmaster.ca>
2019-02-15 04:43:11 +0000

    2nd edition of project_analyze and README & added test files and directories

--- Project01/README.md
+++ Project01/README.md
@@ -1,12 +1,12 @@
 FEATURES:
 1.The script is interactive by user prompted input
 2.Puts each line of every file in my repo with the tag #TODO into todo.log
-3.Find Haskell and Python files in my repo thath fail to compile and put them in compile_fail.log
+3.Find Haskell and Python files in my repo that fail to compile and put them in compile_fail.log
 4.Find all commit hashes where merge is mentioned in the commit messages and put them in merge.log
 5.
 6.
 HOW TO USE MY SCRIPT:
 a. follow instructions
-b. type the feature you want to execute as described in this file
+b. type the feature you want to execute as described in this file(i.e. type 2 to execute feature 2, etc.)
 c. type q to quit
 
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,3 +1,4 @@
+cd ..
 feature=''
 while [ "$feature" != 'q' ]; do
     echo 'which feature do you want to execute[2,3,4,q]'
@@ -5,19 +6,21 @@ while [ "$feature" != 'q' ]; do
     if [ "$feature" = '2' ]; then
       find . -type f -print0 | while IFS= read -d $'\0' file
         do
-          grep -E '#TODO' $file >> todo.log
+	  if [ "$file" != "*$0" ]; then
+         	 grep -E '#TODO' "$file" >> Project01/logs/todo.log
+	  fi
         done
     elif [ "$feature" = '3' ]; then
-      find . -name *.py -print0 | while IFS= read -d $'\0' file
+      find . -name "*.py" -print0 | while IFS= read -d $'\0' file
         do
-          python "$file" 2>> compile_fail.log
+          python "$file" 2>> Project01/logs/compile_fail.log
         done
-      find . -name *.hs -print0 | while IFS= read -d $'\0' file
+      find . -name "*.hs" -print0 | while IFS= read -d $'\0' file
         do
-          runhaskell "$file" 2>> compile_fail.log
+          runhaskell "$file" 2>> Project01/logs/compile_fail.log
         done
     elif [ "$feature" = '4' ]; then
-      git log -oneline | cut -d'' -f1 | grep -E 'merge' >> merge.log
+      git log --oneline | grep -E -i 'merge' | cut -d' ' -f1  >> Project01/logs/merge.log
     fi
 done
 
