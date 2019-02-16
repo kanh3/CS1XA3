8d325bc8865a0efcb21a0320163e9568fa51532b
Hailan Kan <kanh3@mcmaster.ca>
2019-02-16 02:08:14 +0000

    3rd edition of README and project_analyze

--- Project01/README.md
+++ Project01/README.md
@@ -1,12 +1,17 @@
-FEATURES:
-1.The script is interactive by user prompted input
-2.Puts each line of every file in my repo with the tag #TODO into todo.log
-3.Find Haskell and Python files in my repo that fail to compile and put them in compile_fail.log
-4.Find all commit hashes where merge is mentioned in the commit messages and put them in merge.log
+**FEATURES**:
+1. The script is interactive by *user prompted input*.
+2. Puts each line with the tag #TODO of every file in my repo into todo.log.
+3. Find Haskell and Python files in my repo that fail to compile and put them 
+in compile_fail.log.
+4. Find all commit hashes where merge is mentioned in the commit messages and 
+put them in merge.log.
 5.
 6.
-HOW TO USE MY SCRIPT:
-a. follow instructions
-b. type the feature you want to execute as described in this file(i.e. type 2 to execute feature 2, etc.)
-c. type q to quit
+**HOW TO USE MY SCRIPT**:
+* run script
+* follow instructions
+* choices are given in the square bracket
+    * type the feature you want to execute as described in this file(i.e. type
+ 2 to execute feature 2, etc.)
+    * type q to quit
 
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -4,23 +4,35 @@ while [ "$feature" != 'q' ]; do
     echo 'which feature do you want to execute[2,3,4,q]'
     read feature
     if [ "$feature" = '2' ]; then
-      find . -type f -print0 | while IFS= read -d $'\0' file
-        do
-	  if [ "$file" != "*$0" ]; then
-         	 grep -E '#TODO' "$file" >> Project01/logs/todo.log
-	  fi
-        done
+	if [ -e Project01/logs/todo.log ]; then
+		rm Project01/logs/todo.log
+	fi
+        find . -type f -print0 | while IFS= read -d $'\0' file
+       	    do
+         		 grep -E '#TODO' "$file" >> Project01/logs/todo.log
+       	    done
+
+
     elif [ "$feature" = '3' ]; then
-      find . -name "*.py" -print0 | while IFS= read -d $'\0' file
-        do
-          python "$file" 2>> Project01/logs/compile_fail.log
-        done
-      find . -name "*.hs" -print0 | while IFS= read -d $'\0' file
-        do
-          runhaskell "$file" 2>> Project01/logs/compile_fail.log
-        done
+	if [ -e Project01/logs/compile_fail.log ]; then
+                rm Project01/logs/compile_fail.log
+	fi
+        find . -name "*.py" -print0 | while IFS= read -d $'\0' file
+            do
+         	 python "$file" 2>> Project01/logs/compile_fail.log
+       	    done
+        find . -name "*.hs" -print0 | while IFS= read -d $'\0' file
+       	    do
+         	 runhaskell "$file" 2>> Project01/logs/compile_fail.log
+       	    done
+
     elif [ "$feature" = '4' ]; then
-      git log --oneline | grep -E -i 'merge' | cut -d' ' -f1  >> Project01/logs/merge.log
+	if [ -e Project01/logs/merge.log ]; then
+                rm Project01/logs/merge.log
+	fi
+        git log --oneline | grep -E -i 'merge' | cut -d' ' -f1  >> Project01/logs/merge.log
+
+
     fi
 done
 
