98f9ba3e4a679343199c04848e80cf191c1f0953
Hailan Kan <kanh3@mcmaster.ca>
2019-04-12 04:06:06 +0000

    login and model

--- Project03/django_project/p3app/urls.py
+++ Project03/django_project/p3app/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.http import HttpResponse
 
 urlpatterns = [
-    path('' , views. , name= 'p3app-'),
+    path('register/' , views.register_user , name= 'p3app-register_user'),
+    path('login/', views.login_user, name="p3app-login_user"),
+    path('userinfo/' views.user_info, name="p3app-user_info"),
 ]
